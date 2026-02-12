1f282cb78d3e4208c11284617e1952dc73266de9
bertranddelvaux <80839599+bertranddelvaux@users.noreply.github.com>
2026-02-12 18:02:07 +0000

    Update event.py
    update_events_to_jsons
    adding the ongoing event to country

--- utils/event.py
+++ utils/event.py
@@ -2,9 +2,11 @@ import datetime as dt
 
 import copy
 
+import os
+
 from utils.json import createJSONifNotExists, jsonFileToDict, dictToJSONFile
 
-from constants.constants import DICT_DEFAULT_VALUES
+from constants.constants import DICT_DEFAULT_VALUES, DATA_FOLDER, EVENTS_FOLDER
 
 def initialize_event(json_path: str, json_file: str, json_dict_update: dict, ongoing_year: str = None, ongoing_month: str = None, ongoing_day: str = None) -> dict:
     """
@@ -84,3 +86,67 @@ def save_json_last_edit(json_path, json_file, json_dict) -> dict:
     dictToJSONFile(json_path=json_path, json_file=json_file, json_dict=json_dict)
 
     return json_dict
+
+def update_event_to_jsons(event, year, country) -> tuple[dict, dict, dict]:
+    """
+    Update event in jsons
+    :param event:
+    :param year:
+    :param country:
+    :return:
+    """
+
+    # Getting data from tupples
+    json_path_event, json_file_event, dict_event = event
+    json_path_year, json_file_year, dict_year = year
+    json_path_country, json_file_country, dict_country = country
+
+    # updating event json
+    dict_event = save_json_last_edit(
+        json_path=json_path_event,
+        json_file=json_file_event,
+        json_dict=dict_event
+    )
+
+    # get event information
+    start_date = dict_event['start_date']
+    year_ongoing = dict_event['ongoing_event_year']
+    month_ongoing = dict_event['ongoing_event_month']
+    day_ongoing = dict_event['ongoing_event_day']
+
+    # updating the event in dict_year
+    dict_year['event_by_event'][start_date] = {
+        'path': os.path.join(json_path_country, EVENTS_FOLDER, year_ongoing,
+                             month_ongoing, day_ongoing, json_file_event),
+        'event': dict_event
+    }
+    dict_year['total_events_year'] = len(dict_year['event_by_event'])
+    dict_year['total_days_year'] = sum([events['event']['total_days_event'] for events in dict_year['event_by_event'].values()])
+
+    # updating the event in dict_country
+    dict_country['year_by_year'].setdefault(year_ongoing, {})
+    dict_country['year_by_year'][year_ongoing][start_date] = dict_year['event_by_event'][start_date]
+
+    # computing number of events and of days
+    all_events = [
+        data['event']['total_days_event']
+        for year in dict_country.get('year_by_year', {}).values()
+        for data in year.values()
+        if 'event' in data
+    ]
+    dict_country['total_events_country'] = len(all_events)
+    dict_country['total_days_country'] = sum(all_events)
+
+    # adding the ongoing event to country
+    if dict_country['ongoing']:
+        dict_country['ongoing_event'] = dict_event
+    else:
+        dict_country['ongoing_event'] = {}
+
+    # updating country json
+    dict_country = save_json_last_edit(json_path_country, json_file_country, dict_country)
+
+    # updating year json
+    dict_year = save_json_last_edit(json_path_year, json_file_year, dict_year)
+
+    return dict_event, dict_year, dict_country
